54335323d9489b3cea2311776aaed4eb9992aebf
PreyingBrantus <32892412+PreyingBrantus@users.noreply.github.com>
2018-05-09 15:46:52 +1000

    Created ItemPropertiesImporter class prototype
    Created a prototype class "ItemPropertiesImporter" to parse the item
    properties CSV file and return a list of Item objects
    
    Only works with the full local file path, need to refactor to use a file
    from the src

--- src/assignment2/Item.java
+++ src/assignment2/Item.java
@@ -2,27 +2,23 @@ package assignment2;
 
 public class Item {
 
+	String itemName;
 	double itemCost;
 	double itemPrice;
-	double itemTemperature;
-	int itemAmount;
-	String itemName;
-	
-	
-	//constructor maybe will add more things along the way
-	Item( double itemCost, double itemPrice, 
-		  double itemTemperature, int itemAmount, 
-		  String itemName){
-		
+	int reorderPoint;
+	int reorderAmount;
+	double itemTemp;
+
+	// constructor maybe will add more things along the way
+	Item(String itemName, double itemCost, double itemPrice, int reorderPoint, int reorderAmount, int itemTemp) {
+
+		this.itemName = itemName;
 		this.itemCost = itemCost;
 		this.itemPrice = itemPrice;
-		this.itemTemperature = itemTemperature;
-		this.itemAmount = itemAmount;
-		this.itemName = itemName;
+		this.reorderPoint = reorderPoint;
+		this.reorderAmount = reorderAmount;
+		this.itemTemp = itemTemp;
+
 	}
-	
-	
-	
-	
-	
+
 }
